7f66412626d92d0f3977c56a89c4c31c617ae43a
Caden Walker <spidermandabest1962@gmail.com>
2023-09-28 18:37:04 +0000

    <message about commit>

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -39,30 +39,23 @@ $(function () {
     //////////ONLY CHANGE BELOW THIS POINT///////////
     /////////////////////////////////////////////////
 
-    // TODO 1
-    // Create platforms
-    // You must decide the x position, y position, width, and height of the platforms
-    // example usage: createPlatform(x,y,width,height)
-
-
-
-    
-    // TODO 2
-    // Create collectables
-    // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
-    // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
-    // example usage: createCollectable(type, x, y, gravity, bounce)
-
-
-
-
-    // TODO 3
-    // Create cannons
-    // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
-    // Your wall choices are: 'top' 'left' 'right' and 'bottom'
-    // example usage: createCannon(side, position, delay, width, height)
-
-
+createCollectable("steve", 500, 170, 6, 0.7); //your example function call
+createPlatform(500, 650, 200, 20);
+createPlatform(10, 650, 150, 15)
+createPlatform(350, 550, 200, 20)
+createPlatform(650, 450, 300, 20)
+createPlatform(550, 330, 200, 20)
+createPlatform(150, 195, 350, 20)
+createPlatform(1000, 550, 200, 20)
+createPlatform(1200, 350, 200, 20)
+createCollectable("steve", 1200, 170, 6, 0.7)
+createCollectable("steve", 550, 170, 6, 0.7)
+createCollectable("steve", 1000, 170, 6, 0.7)
+createCannon("left", 350, 1200);
+createCannon("top", 700, 1200);
+createCannon("right", 300, 1200);
+createCannon("bottom", 350, 1200);
+createCannon("left", 350, 1200);
 
 
     /////////////////////////////////////////////////
