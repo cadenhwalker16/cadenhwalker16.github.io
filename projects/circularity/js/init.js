7eb89d6d07b81fad7862913c48e140dca12205cd
Caden Walker <spidermandabest1962@gmail.com>
2024-02-26 19:41:24 +0000

    refactor to create 100 circles using loops

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -32,7 +32,7 @@ var init = function (window) {
         }
 
         // TODO 3 / 7 : Call the drawCircle() function 
-            for(var i = 0; i < 100; i++){
+            for(var i = 0; i < 1000; i++){
                     drawCircle();
             }
 
@@ -72,19 +72,19 @@ var init = function (window) {
         game.checkCirclePosition = function(circle) {
 
             // if the circle has gone past the RIGHT side of the screen then place it on the LEFT
-            if ( circle.x > canvas.width ) {
+            if ( circle.x - circle.radius > canvas.width ) {
                 circle.x = 0;
             }
 
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            if ( circle.y > canvas.height){
+            if ( circle.y - circle.radius > canvas.height){
                 circle.y = 0;
             }
-            if ( circle.x < 0){
+            if ( circle.x + circle.radius < 0){
                 circle.x = canvas.width;
             }
-            if ( circle.y < 0){
+            if ( circle.y + circle.radius < 0){
                 circle.y = canvas.height;
             }
 
